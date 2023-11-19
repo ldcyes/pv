19d2846e3953dc93afec901014e6953d9e3007a3
ldcyes <ldcyes@163.com>
2023-11-19 23:08:50 +0800

    next week month index

--- global_var.py
+++ global_var.py
@@ -5,3 +5,6 @@ buy_position = [10,20,30,40]
 sell_position = [10,20,30,40]
 stock_keys=['QQQ','SOXX','NVDA','TSLA']#'中芯国际']
 start_date = '20120617'
+
+test_start_date = '20201001'
+test_end_date = '20230330'
