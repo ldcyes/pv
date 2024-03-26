77e5b9b31c8df2409e1442ce1a5ecdcd87efd990
ldcyes <ldcyes@163.com>
2024-03-26 22:29:03 +0800

    test day update

--- 10_percent_model/tenrise_predict_today.py
+++ 10_percent_model/tenrise_predict_today.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 def predict_now():
     current_date = datetime.now()
-    formatted_date = current_date.strftime('%Y%m%d')
+    formatted_date = current_date.strftime('%Y-%m-%d')
 
     df_org = pd.read_csv("./stock_data/10rise_TEST_DATA.csv")
 
@@ -25,7 +25,7 @@ def predict_now():
     filtered_df= df_org[features_remain][0:-1]
     print("with NA value data shape")
     print(filtered_df.shape)
-    test_date='2024-03'#str(formatted_date)
+    test_date=str(formatted_date)
     #test_df_new = filtered_df[filtered_df['date'].dt.strftime('%Y-%m').str.contains('2024-03')]
     filtered_df=filtered_df.replace([np.inf, -np.inf], np.nan).dropna(subset=features_x)
     
